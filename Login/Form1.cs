a7fc0d33436ed205d32fb4763ffb4e75493f8b68
SARAH DOS SANTOS COSTA <sarah.scosta3@senacsp.edu.br>
2025-02-27 22:11:07 -0300

    Aula Neymar

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -2,46 +2,59 @@ namespace Login
 {
     public partial class Form1 : Form
     {
+        List<string> listaUsuarios = new List<string>() { "neymar.jr", "pablo vittar", "sukuna.silva" };
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        { }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string usuário = textBoxUsuário.Text;
-            string senha = textBoxSenha.Text;
+          string usuarioBuscado = textBoxUsuário.Text;
+          string senha = textBoxSenha.Text;
 
-            if (usuário == null || usuário == "")
+            if (string.IsNullOrWhiteSpace(usuarioBuscado))
             {
-                labelResultado.Text = " O Usuário é obrigatório!!!";
-                labelResultado.ForeColor = Color.Red;
-                return;
+                    labelResultado.Text = " O Usuário é obrigatório!!!";
+                    labelResultado.ForeColor = Color.Red;
+                    return;
             }
 
-            else if (senha == null || senha == "")
+            if (senha == null || senha == "")
             {
-                labelResultado.Text = "A senha é obrigatória!!!";
-                labelResultado.ForeColor = Color.Red;
-                return;
+                    labelResultado.Text = "A senha é obrigatória!!!";
+                    labelResultado.ForeColor = Color.Red;
+                    return;
             }
 
-            if (usuário == "Sarah" && senha == "12345")
+            int posicaoUsuarioEncontrado = -1;
+
+               for (int i = 0; i < listaUsuarios.Count; i++)
+               {
+
+                    if (usuarioBuscado == listaUsuarios[i])
+                    {
+                       posicaoUsuarioEncontrado = i;
+                    }
+               }
+
+
+              
+            if (posicaoUsuarioEncontrado > -1 && senha == "12345")
             {
-                labelResultado.Text = "Autenticado com sucesso!";
-                labelResultado.ForeColor = Color.Green;
+                    labelResultado.Text = "Autenticado com sucesso!";
+                    labelResultado.ForeColor = Color.Green;
             }
             else
             {
-                labelResultado.Text = "Usuário ou senha incorretos...";
-                labelResultado.ForeColor = Color.Red;
+                    labelResultado.Text = "Usuário ou senha incorretos...";
+                    labelResultado.ForeColor = Color.Red;
             }
-
+            
 
         }
 
--- Login/obj/Debug/net8.0-windows/Login.AssemblyInfo.cs
+++ Login/obj/Debug/net8.0-windows/Login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed1f2b19e456d3d92aa82aba1479e2fe91a4a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898e005692ab1d7d6b972c2430dd443aae774cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
