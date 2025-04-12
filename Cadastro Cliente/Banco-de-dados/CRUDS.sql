64b584eecb77449fdbd9a707fc914be66ebc2293
SARAH DOS SANTOS COSTA <sarah.scosta3@senacsp.edu.br>
2025-04-11 22:21:31 -0300

    Lista de Atividades

---
+++ Cadastro Cliente/Banco-de-dados/CRUDS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE atividade (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR (100) NOT NULL,
+    situacao INT NOT NULL default 0
+);
+     
+INSERT INTO atividade (titulo) VALUES (@titulo);
+
+UPDATE atividade SET situacao = @situacao WHERE id = @id;
+
+SELECT * FROM atividade WHERE situacao = 1;
--- Cadastro Cliente/ClassEnderecoCliente.cs
+++ Cadastro Cliente/ClassEnderecoCliente.cs
@@ -8,6 +8,7 @@ namespace Cadastro_Cliente
 {
     internal class EnderecoCliente
     {
+        public int id { get; set; }
         public string logradouro {  get; set; }
         public string numero {  get; set; }
         public string complemento { get; set; }
