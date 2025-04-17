fc0209f41c06f678e3ecc3b8cfab06db91bb89f0
SARAH DOS SANTOS COSTA <sarah.scosta3@senacsp.edu.br>
2025-04-17 19:53:46 -0300

    Trabalho em Equipe

--- Trabalho em equipe/Trabalho em equipe.sql
+++ Trabalho em equipe/Trabalho em equipe.sql
@@ -1,13 +1,44 @@
-CREATE TABLE IF NOT EXISTS produto (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-		nome VARCHAR(100) NOT NULL,
-		data_tempo DATETIME NOT NULL
-        );
-        
-CREATE TABLE IF NOT EXISTS estoque (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-        quantidade VARCHAR(20) NOT NULL,
-        id_produto INT NOT NULL,
-        data_tempo DATETIME NOT NULL
-        );
-        
+	CREATE TABLE IF NOT EXISTS produto (
+			id INT PRIMARY KEY AUTO_INCREMENT,
+			nome VARCHAR(100) NOT NULL,
+			data_tempo DATETIME NOT NULL
+			);
+			
+	CREATE TABLE IF NOT EXISTS estoque (
+			id INT PRIMARY KEY AUTO_INCREMENT,
+			quantidade VARCHAR(20) NOT NULL,
+			id_produto INT NOT NULL,
+			data_tempo DATETIME NOT NULL,
+			FOREIGN KEY (id_produto)
+			REFERENCES produto (id)
+			);
+			
+		
+	CREATE TABLE IF NOT EXISTS vendas (
+			id INT PRIMARY KEY AUTO_INCREMENT,
+			id_produto INT NOT NULL,
+			id_estoque INT NOT NULL,
+			data_tempo DATETIME NOT NULL,
+			FOREIGN KEY (id_produto)
+			REFERENCES produto (id),
+			FOREIGN KEY (id_estoque)
+			REFERENCES estoque (id)
+			);
+
+	INSERT INTO produto (
+		nome,
+		data_tempo
+		) 
+	VALUES 
+		(@nome, @data_tempo);
+
+	DELETE FROM produto WHERE id = @id;
+
+	INSERT INTO estoque (
+		quantidade,
+        id_produto,
+        data_tempo
+        )
+	VALUES 
+		(@quantidade, @id_produto, @data_tempo);
+
